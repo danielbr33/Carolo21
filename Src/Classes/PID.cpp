ca7729c29e24942369b2069e1dd4132a5bb09e9f
danie <danie@LAPTOP-A0QUV2M1>
2020-03-07 14:21:55 +0100

    Przypisanie kp,kd,ki

--- Src/Classes/PID.cpp
+++ Src/Classes/PID.cpp
@@ -7,20 +7,42 @@
 
 #include <PID.h>
 
-	uint16_t PID::getOutput()
-	{
-
-	}
-void PID::setKp(uint16_t Kp)
+uint16_t PID::getOutput()
 {
 
 }
-PID::PID() {
-	// TODO Auto-generated constructor stub
 
+void PID::setKp(uint16_t Kp)
+{
+	if (Kp>100)
+		kp=100;
+	else
+		kp=Kp;
 }
 
-PID::~PID() {
+void PID::setKd(uint16_t Kd)
+{
+	if (Kd>100)
+		kd=100;
+	else
+		kd=Kd;
+}
+
+void PID::setKi(uint16_t Ki)
+{
+	if (Ki>100)
+		ki=100;
+	else
+		ki=Ki;
+}
+
+PID::PID()
+{
+	// TODO Auto-generated constructor stub
+}
+
+PID::~PID()
+{
 	// TODO Auto-generated destructor stub
 }
 
--- Src/Classes/PID.h
+++ Src/Classes/PID.h
@@ -14,12 +14,16 @@ class PID {
 public:
 	PID();
 	void setKp(uint16_t Kp);
+	void setKd(uint16_t Kd);
+	void setKi(uint16_t Ki);
 	uint16_t getOutput();
 	virtual ~PID();
 private:
 	uint16_t kp;
 	uint16_t ki;
 	uint16_t kd;
+	uint16_t output;
+	void pid();
 };
 
 
