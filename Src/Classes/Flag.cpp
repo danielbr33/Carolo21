44fb744a955f813b9e951d7eb2e0e237af2a6666
danie <danie@LAPTOP-A0QUV2M1>
2020-03-08 21:49:12 +0100

    Stworzenie tablicy wskazujacej na obiekty i blad

--- Src/Classes/Flag.cpp
+++ Src/Classes/Flag.cpp
@@ -7,10 +7,19 @@
 
 #include <Flag.h>
 
+
 Flag::Flag(string name)
 {
+
 	// TODO Auto-generated constructor stub
 	this->name=name;
+	numberOfFlags+=1;
+    Flag ** newTab = new Flag*[numberOfFlags];
+    for (uint8_t i=0; i<numberOfFlags; i++)
+    	newTab[i]=tab[i];
+    newTab[numberOfFlags]=this;
+    tab=newTab;  //w tej linijce jest blad, jak wstawic wartosci z tymczasowej tablicy do tablicy statycznej klasy
+    delete[] newTab;
 }
 
 Flag::~Flag() {
@@ -26,3 +35,9 @@ uint16_t Flag::getValue()
 {
 	return value;
 }
+
+ uint16_t Flag::getNumberOfFlags()
+{
+	return numberOfFlags;
+}
+
--- Src/Classes/Flag.h
+++ Src/Classes/Flag.h
@@ -17,11 +17,14 @@ class Flag
 private:
 	string name;
 	uint16_t value;
+	uint16_t numberOfFlags=0;
+	static Flag** tab;
 public:
 	Flag(string name);
 	virtual ~Flag();
 	void setValue(uint16_t value);
 	uint16_t getValue();
+	uint16_t getNumberOfFlags();
 };
 
 #endif /* CLASSES_FLAG_H_ */
